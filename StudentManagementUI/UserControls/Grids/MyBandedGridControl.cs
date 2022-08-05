75826f57a5815fb3b3f39d6a1ef768073501ee9e
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-08-05 10:24:21 +0300

    ..

---
+++ StudentManagementUI/UserControls/Grids/MyBandedGridControl.cs
@@ -0,0 +1,160 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Mask;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Registrator;
+using DevExpress.XtraGrid.Views.BandedGrid;
+using DevExpress.XtraGrid.Views.Base;
+using StudentManagementUI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagementUI.UserControls.Grids
+{
+
+    [ToolboxItem(true)]
+    public class MyBandedGridControl : GridControl
+    {
+
+        protected override BaseView CreateDefaultView()
+        {
+
+            var view = (BandedGridView)CreateView("MyBandedGridView");
+            //This is our Band Color we set into DarkBlue
+            view.Appearance.BandPanel.ForeColor = Color.DarkBlue;
+            //This is our Font of Band text
+            view.Appearance.BandPanel.Font = new Font(new FontFamily("Tahoma"), 8.25F, FontStyle.Bold);
+            //Our Band text we made it as horz center
+            view.Appearance.BandPanel.TextOptions.HAlignment = HorzAlignment.Center;
+            //This is Height of our Band Text
+            view.BandPanelRowHeight = 40;
+
+            view.Appearance.HeaderPanel.ForeColor = Color.Maroon;
+            view.Appearance.HeaderPanel.TextOptions.HAlignment = HorzAlignment.Center;
+            view.Appearance.ViewCaption.ForeColor = Color.Maroon;
+
+            view.Appearance.FooterPanel.ForeColor = Color.Maroon;
+            view.Appearance.FooterPanel.Font = new Font(new FontFamily("Tahoma"), 8.25F, FontStyle.Bold);
+
+            view.OptionsMenu.EnableColumnMenu = false;
+            view.OptionsMenu.EnableFooterMenu = false;
+            view.OptionsMenu.EnableGroupPanelMenu = false;
+
+            view.OptionsNavigation.EnterMoveNextColumn = true;
+
+            view.OptionsPrint.AutoWidth = false;
+            view.OptionsPrint.PrintFooter = false;
+            view.OptionsPrint.PrintGroupFooter = false;
+
+            view.OptionsView.ShowAutoFilterRow = true;
+            view.OptionsView.ShowViewCaption = true;
+            view.OptionsView.ShowGroupPanel = false;
+            view.OptionsView.ColumnAutoWidth = false;
+            view.OptionsView.RowAutoHeight = true;
+            view.OptionsView.HeaderFilterButtonShowMode = FilterButtonShowMode.Button;
+
+            var idColumn = new BandedGridColumn();
+            idColumn.Caption = "Id";
+            idColumn.FieldName = "Id";
+            idColumn.OptionsColumn.AllowEdit = false;
+            idColumn.OptionsColumn.ShowInCustomizationForm = false;
+            view.Columns.Add(idColumn);
+
+            var codeColumn = new BandedGridColumn();
+            codeColumn.Caption = "Private Code";
+            codeColumn.FieldName = "PrivateCode";
+            codeColumn.Visible = true;
+            codeColumn.OptionsColumn.AllowEdit = false;
+            codeColumn.AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+            codeColumn.AppearanceCell.Options.UseTextOptions = true;
+            view.Columns.Add(codeColumn);
+
+            return view;
+
+        }
+
+        protected override void RegisterAvailableViewsCore(InfoCollection collection)
+        {
+            base.RegisterAvailableViewsCore(collection);
+            collection.Add(new MyBandedGridInfoRegistrator());
+        }
+
+        private class MyBandedGridInfoRegistrator : BandedGridInfoRegistrator
+        {
+            public override string ViewName => "MyBandedGridView";
+            public override BaseView CreateView(GridControl grid)
+            {
+                return new MyBandedGridView(grid);
+            }
+        }
+    }
+
+    public class MyBandedGridView : BandedGridView, IStatusBarShortcut
+    {
+
+
+        public MyBandedGridView()
+        {
+
+        }
+
+        public MyBandedGridView(GridControl ownerGrid) : base(ownerGrid)
+        {
+
+        }
+
+        protected override void OnColumnChangedCore(GridColumn column)
+        {
+            base.OnColumnChangedCore(column);
+            if (column.ColumnEdit == null) return;
+            if (column.ColumnEdit.GetType() == typeof(RepositoryItemDateEdit))
+            {
+                column.AppearanceCell.TextOptions.HAlignment = HorzAlignment.Center;
+                ((RepositoryItemDateEdit)column.ColumnEdit).Mask.MaskType = MaskType.DateTimeAdvancingCaret;
+            }
+        }
+
+        protected override GridColumnCollection CreateColumnCollection()
+        {
+            return new MyGridColumnCollection(this);
+        }
+
+
+
+        private class MyGridColumnCollection : BandedGridColumnCollection
+        {
+            public MyGridColumnCollection(ColumnView view) : base(view)
+            {
+
+            }
+
+            protected override GridColumn CreateColumn()
+            {
+                var column = new MyBandedGridColumn();
+                column.OptionsColumn.AllowEdit = false;
+                return column;
+            }
+        }
+
+
+        public string StatusBarShortcut { get; set; }
+        public string StatusBarShortcutDescription { get; set; }
+        public string StatusBarDescription { get; set; }
+    }
+
+    public class MyBandedGridColumn : BandedGridColumn, IStatusBarShortcut
+    {
+
+        public string StatusBarShortcut { get; set; }
+        public string StatusBarShortcutDescription { get; set; }
+        public string StatusBarDescription { get; set; }
+    }
+
+}
