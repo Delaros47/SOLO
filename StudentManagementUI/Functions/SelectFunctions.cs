f42cb8b01bedb52fa47abb0350cc6eb465f49f83
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-08-11 17:28:22 +0300

    ..

--- StudentManagementUI/Functions/SelectFunctions.cs
+++ StudentManagementUI/Functions/SelectFunctions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Common.Enums;
+using StudentManagementUI.UserControls.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,17 +8,43 @@ using System.Threading.Tasks;
 
 namespace StudentManagementUI.Functions
 {
+    #region Comment
+    /*
+     * This class will basiclly takes Id and Values from the Table (GridView) to our ButtonEdit controls
+     * 
+     */
+    #endregion
     public class SelectFunctions : IDisposable
     {
+        #region Comment
+        /*
+         * we have declared two buttonEdits one for example for City and second for District because District one is connected with City one
+         * 
+         */
+        #endregion
+        private MyButtonEdit _btnEdit;
+        private MyButtonEdit _prmEdit;
+        private FormType _formType;
+
+        public void Selection(MyButtonEdit btnEdit)
+        {
+            _btnEdit = btnEdit;
+            SelectionProccess();
+        }
+
+        public void Selection(MyButtonEdit btnEdit,MyButtonEdit prmEdit)
+        {
+            _btnEdit = btnEdit;
+            _prmEdit = prmEdit;
+            SelectionProccess();
+        }
 
 
 
+        public void SelectionProccess()
+        {
 
-
-
-
-
-
+        }
 
 
 
