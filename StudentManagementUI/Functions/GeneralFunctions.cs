1c02ff280e2b3e3bf2c33820a94b43780f76586d
ahmet hamdi ahmetoglu <Ajdihak_med@hotmail.com>
2022-08-19 02:25:59 +0300

    ...

--- StudentManagementUI/Functions/GeneralFunctions.cs
+++ StudentManagementUI/Functions/GeneralFunctions.cs
@@ -5,10 +5,6 @@ using DevExpress.XtraGrid.Views.Grid;
 using Model.Entities.Base;
 using StudentManagementUI.UserControls.Controls;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace StudentManagementUI.Functions
